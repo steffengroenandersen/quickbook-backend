212e9b65eeaad48721091dfc9c3ed9701974462b
steffengroenandersen <steffengroenandersen@gmail.com>
2024-01-12 07:47:08 +0100

    Add unittests

--- src/test/java/com/quickbook/quickbookbackend/service/HotelServiceTest.java
+++ src/test/java/com/quickbook/quickbookbackend/service/HotelServiceTest.java
@@ -1,84 +1,54 @@
 package com.quickbook.quickbookbackend.service;
 
+import com.quickbook.quickbookbackend.dto.HotelRequest;
 import com.quickbook.quickbookbackend.dto.HotelResponse;
 import com.quickbook.quickbookbackend.entity.Hotel;
 import com.quickbook.quickbookbackend.repository.HotelRepository;
+import com.quickbook.quickbookbackend.repository.ReservationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class HotelServiceTest {
 
     HotelService hotelService;
-    
     @Mock
     HotelRepository hotelRepository;
-    
+    @Mock
+    ReservationRepository reservationRepository;
+
     @BeforeEach
-    void setUp() {
-        // Arrange
-        hotelService = new HotelService(hotelRepository);
-        
-        List<Hotel> mockHotels = new ArrayList<>(Arrays.asList(
-                new Hotel("Hotel A", "123 Main St", "City A", 12345, "Country A"),
-                new Hotel("Hotel B", "456 Oak St", "City B", 67890, "Country B")
-        ));
-
-        
-        when(hotelRepository.findAll()).thenReturn(mockHotels);
-        
-        when(hotelRepository.findById(1)).thenReturn(Optional.of(mockHotels.get(1)));
+    void setUp(){
+        hotelService = new HotelService(hotelRepository, reservationRepository);
     }
-
+    
+    Hotel mockHotel(){
+        Hotel mockHotel = new Hotel("mockName","mockStreet","mockCity",9999,"mockCountry");
+        return mockHotel;
+    }
+    
     @Test
     void createHotel() {
-    }
+        // Arrange
+        Hotel mockHotel = mockHotel();
+        HotelRequest hotelRequest = new HotelRequest(mockHotel.getName(),
+                mockHotel.getStreet(),
+                mockHotel.getCity(), 9999,
+                mockHotel.getCountry(), 1);
+        when(hotelRepository.save(any(Hotel.class))).thenReturn(mockHotel);
 
-    @Test
-    void getAllHotels_ReturnsAllHotelsAsHotelResponses() {
         // Act
-        List<HotelResponse> hotelResponses = hotelService.getAllHotels();
-        
+        HotelResponse hotelResponse = hotelService.createHotel(hotelRequest);
+
         // Assert
-        assertEquals(2, hotelResponses.size(),"The getAllHotels method should find all hotels and return as HotelResponses");
-
-        for (HotelResponse hotelResponse : hotelResponses) {
-            assertTrue(hotelResponse instanceof HotelResponse, "Each element should be an instance of HotelResponse");
-        }
-    }
-
-    @Test
-    void getHotel_ReturnsHotelResponseById() {
-        // Act
-        HotelResponse hotelResponse1 = hotelService.getHotel(1);
-        
-        // Assert
-        assertNotNull(hotelResponse1);
-        assertEquals("Hotel B", hotelResponse1.getName());
-
-
-    }
-
-    @Test
-    void updateHotel() {
-    }
-
-    @Test
-    void deleteHotel() {
-    }
-
-    @Test
-    void createRoom() {
+        verify(hotelRepository, times(1)).save(any(Hotel.class));
     }
 }
--- src/test/java/com/quickbook/quickbookbackend/service/ReservationServiceTest.java
+++ src/test/java/com/quickbook/quickbookbackend/service/ReservationServiceTest.java
@@ -1,9 +1,7 @@
 package com.quickbook.quickbookbackend.service;
 
-import com.quickbook.quickbookbackend.dto.HotelResponse;
 import com.quickbook.quickbookbackend.dto.ReservationResponse;
 import com.quickbook.quickbookbackend.entity.Guest;
-import com.quickbook.quickbookbackend.entity.Hotel;
 import com.quickbook.quickbookbackend.entity.Reservation;
 import com.quickbook.quickbookbackend.entity.Room;
 import com.quickbook.quickbookbackend.repository.GuestRepository;
@@ -14,130 +12,118 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import org.springframework.web.server.ResponseStatusException;
+import java.security.Principal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class ReservationServiceTest {
 
     ReservationService reservationService;
-    
     @Mock
     ReservationRepository reservationRepository;
-    
     @Mock
     HotelService hotelService;
-    
     @Mock
     GuestRepository guestRepository;
-    
     @Mock
     RoomRepository roomRepository;
-    
+  
     @BeforeEach
-    void setUp() {
-        reservationService = new ReservationService(reservationRepository, hotelService, guestRepository, roomRepository);
+    void setUp(){
+        reservationService = new ReservationService(reservationRepository, hotelService,guestRepository,roomRepository);
     }
     
-    Guest generateTestUser(){
-        return new Guest("testUser1", "testUser@localhost.com", "pass", "Steffen", "Andersen");
+    Guest mockGuest(){
+        Guest mockGuest = new Guest("mockUsername", "mockEmail", "mockPass", "mockFirstName", "mockLastName");
+        guestRepository.save(mockGuest);
+        return mockGuest;
     }
     
-    Hotel generateTestHotel(){
-        return new Hotel("Test Hotel", "123 Main St", "Test City", 12345, "Test Country");
+    Room mockRoom(){
+        Room mockRoom = new Room(1,2);
+        roomRepository.save(mockRoom);
+        return mockRoom;
     }
     
-    Room generateTestRoom(){
-        return new Room(1, 2);
-    }
-    
-    Reservation generateReservation(Guest testGuest, Room testRoom, LocalDate reservationDate ){
-        return new Reservation(testGuest, testRoom, reservationDate);
+    Reservation mockReservation(Guest mockGuest, Room mockRoom){
+        LocalDate reservationDate = LocalDate.now();
+        Reservation testReservation = new Reservation(mockGuest, mockRoom, reservationDate);
+        return testReservation;
     }
     
     @Test
-    void getAllReservations_ReturnsAllReservationsAsReservationResponses() {
-        
+    void getAllReservations() {
         // Arrange
-        Guest testGuest = generateTestUser();
-
-        Hotel testHotel = generateTestHotel();
-        Room testRoom = generateTestRoom();
-        
-        List<Room> testRoomList = new ArrayList<>();
-        testRoomList.add(testRoom);
-        testHotel.setRooms(testRoomList);
-
-        LocalDate reservationDate = LocalDate.now();
-        Reservation reservation1 = generateReservation(testGuest, testRoom, reservationDate);
-        Reservation reservation2 = generateReservation(testGuest, testRoom, reservationDate);
-        
-        List<Reservation> reservations = new ArrayList<>();
-        reservations.add(reservation1);
-        reservations.add(reservation2);
-        
-        when(reservationRepository.findAll()).thenReturn(reservations);
+        Reservation testReservation = mockReservation(mockGuest(), mockRoom());
+        when(reservationRepository.findAll()).thenReturn(List.of(testReservation));
         
         // Act
-        List<ReservationResponse> testReservations = reservationService.getAllReservations();
+        List<ReservationResponse> reservationResponses = reservationService.getAllReservations();
         
         // Assert
-        assertEquals(2, testReservations.size(), "The getAllReservations method should return 1 reservation");
-        
-        for(ReservationResponse reservationResponse : testReservations){
-            assertTrue(reservationResponse instanceof ReservationResponse,"Each element should be an instance of ReservationResponse");
-        }
+        assertEquals(1, reservationResponses.size());
     }
 
     @Test
-    void getAllReservationsForHotelRoom_ReturnsAllReservationsAsReservationResponses() {
+    void getAllReservationsForHotelRoom() {
         // Arrange
-        Guest testGuest = generateTestUser();
-
-        Hotel testHotel = generateTestHotel();
-        Room testRoom = generateTestRoom();
-
-        List<Room> testRoomList = new ArrayList<>();
-        testRoomList.add(testRoom);
-        testHotel.setRooms(testRoomList);
-
-        LocalDate reservationDate = LocalDate.now();
-        Reservation reservation1 = generateReservation(testGuest, testRoom, reservationDate);
-        Reservation reservation2 = generateReservation(testGuest, testRoom, reservationDate);
-
-        List<Reservation> reservations = new ArrayList<>();
-        reservations.add(reservation1);
-        reservations.add(reservation2);
-        
-        when(reservationRepository.findByRoomId(1)).thenReturn(reservations);
+        Reservation testReservation = mockReservation(mockGuest(), mockRoom());
+        when(reservationRepository.findByRoomId(1)).thenReturn(List.of(testReservation));
         
         // Act
         List<ReservationResponse> reservationResponses = reservationService.getAllReservationsForHotelRoom(1);
         
         // Assert
-        assertEquals(2, reservationResponses.size(), "The getAllReservationsForHotelRoom method should return 2 reservations");
-
-        for(ReservationResponse reservationResponse : reservationResponses){
-            assertTrue(reservationResponse instanceof ReservationResponse,"Each element should be an instance of ReservationResponse");
-        }
+        assertEquals(1, reservationResponses.size());
     }
 
     @Test
     void getAllReservationsForGuest() {
+        // Arrange
+        Guest mockGuest = mockGuest();
+        Reservation testReservation = mockReservation(mockGuest(), mockRoom());
+        when(reservationRepository.findByGuestUsername(mockGuest.getUsername())).thenReturn(List.of(testReservation));
+        
+        // Act
+        List<ReservationResponse> reservationResponses = reservationService.getAllReservationsForGuest(mockGuest.getUsername());
+        
+        // Assert
+        assertEquals(1, reservationResponses.size());
+
     }
 
     @Test
     void deleteReservation() {
-    }
+        // Arrange
+        Guest mockGuest = mockGuest();
+        Reservation testReservation = mockReservation(mockGuest, mockRoom());
+        Principal principal = () -> mockGuest.getUsername();
+        when(reservationRepository.findById(testReservation.getId())).thenReturn(java.util.Optional.of(testReservation));
 
+        // Act
+        ReservationResponse deletedReservation = reservationService.deleteReservation(testReservation.getId(), principal);
+
+        // Assert
+        verify(reservationRepository, times(1)).delete(testReservation);
+    }
+    
     @Test
-    void makeReservation() {
+    void deleteNonExistentReservation() {
+        // Arrange
+        Guest mockGuest = mockGuest();
+        int nonExistentReservationId = 999;
+        Principal principal = () -> mockGuest.getUsername();
+        when(reservationRepository.findById(nonExistentReservationId)).thenReturn(java.util.Optional.empty());
+
+        // Act and Assert
+        assertThrows(ResponseStatusException.class, () -> {
+            reservationService.deleteReservation(nonExistentReservationId, principal);
+        }, "Should throw ResponseStatusException with HttpStatus.NOT_FOUND");
     }
 }
